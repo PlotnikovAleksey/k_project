f12ce041649b794e79a56cce8b3b3a9ad74a4627
Aleha <alex280201@gmail.com>
2019-11-25 23:52:31 +0300

    added .clang-format and googletests

--- tests/simple.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include <kp_analysis_from_dump.hpp>
-
-int main() {
-	char source[] = "dumps/arp_dump.txt";
-	return processing(source);
-}
---
+++ tests/test.cpp
@@ -0,0 +1,16 @@
+﻿#include <gtest/gtest.h>
+#include <kp_analysis_from_dump.hpp>
+
+TEST(Sniffer, ARP) { processing("dumps/arp_dump.txt"); }
+
+TEST(Sniffer, TCP) { processing("dumps/tcp_dump.txt"); }
+
+TEST(Sniffer, particular_TCP_port) { processing("dumps/port443_dump.txt"); }
+
+TEST(Sniffer, UDP) { processing("dumps/udp_dump.txt"); }
+
+TEST(Sniffer, ICMP) { processing("dumps/icmp_dump.txt"); }
+
+TEST(Sniffer, IGMP) { processing("dumps/igmp_dump.txt"); }
+
+TEST(Sniffer, IPv6) { processing("dumps/ipv6_dump.txt"); }
